d945c6d9074c1126512acd6d9910c598cf60afc0
Wisura <joao9.claro99@gmail.com>
2024-03-01 15:48:03 +0000

    Make cards have random pictures
    In order to be playable, a memory game needs that cards have random images each round.

--- js/index.js
+++ js/index.js
@@ -5,6 +5,19 @@ const panelGame = document.querySelector("#game");
 const btLevel = document.querySelector("#btLevel");
 const btPlay = document.querySelector("#btPlay");
 const message = document.querySelector("#message");
+const cards = document.querySelectorAll(".card");
+const cardsLogos = [
+  "angular",
+  "bootstrap",
+  "html",
+  "javascript",
+  "vue",
+  "svelte",
+  "react",
+  "css",
+  "backbone",
+  "ember",
+];
 
 //UseCapture - para propragacao de eventos
 //por Default o event e propragado
@@ -48,10 +61,49 @@ function startGame() {
 
   for (let item of document.querySelectorAll(".list-item"))
     item.classList.add("gameStarted");
+
+  showCards();
+  shuffleArray(cardsLogos);
+  changePictures();
+  let newCardLogos = [];
 }
 
 function stopGame() {
   btPlay.textContent = "Iniciar Jogo";
   btLevel.disabled = false;
   reset();
+  hideCards();
+}
+
+function showCards() {
+  for (let card of cards) {
+    card.classList.add("flipped");
+  }
+}
+
+function hideCards() {
+  for (let card of cards) {
+    card.classList.remove("flipped");
+  }
+}
+
+//Algoritmo Fisher-Yates -> baralhar um array
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
+function changePictures() {
+  let indice = 0;
+  for (const card of cards) {
+    let cardFront = card.querySelector(".card-front");
+    cardFront.src = `images/${cardsLogos[indice]}.png`;
+    card.dataset.logo = cardsLogos[indice];
+
+    indice++;
+  }
 }
